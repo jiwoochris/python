ffc32e329bbdd5ff989a8a07c42ad69308550e1b
juicyjung <travelandi01@gmail.com>
2021-06-17 04:58:33 +0900

    chapter 4

---
+++ 4-2.py
@@ -0,0 +1,14 @@
+score = 65
+
+if score >= 90 and score <= 100 :
+    print("A")
+elif score >=80 and score <90 : 
+    print("B")
+elif score >=70 and score <80 : 
+    print("C")
+elif score >=60 and score <70 : 
+    print("B")
+elif score >=50 and score <60 : 
+    print("B")
+else :
+    print("F")
