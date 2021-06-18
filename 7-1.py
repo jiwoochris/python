7fab3223c0811d8ca9d467606a1aae5691eb854c
juicyjung <travelandi01@gmail.com>
2021-06-18 23:33:59 +0900

    chapter 7

---
+++ 7-1.py
@@ -0,0 +1,7 @@
+import mymath
+
+data = [10, 20, 30]
+
+print(mymath.average(data))
+print(mymath.deviation(data))
+print(mymath.variance(data))
---
+++ mymath.py
@@ -0,0 +1,15 @@
+def average(data):
+    return(sum(data)/len(data))
+
+def deviation(data):
+    dev = []
+    for i in data:
+        dev.append(i-average(data))
+    return dev
+
+def variance(data):
+    total = 0
+    for i in deviation(data):
+        total += i*i
+
+    return total / len(data)
