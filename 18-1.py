c6f896720f5ad56f009d6869eb192f734fbc0a5a
juicyjung <travelandi01@gmail.com>
2021-07-15 02:11:09 +0900

    chapter 18

--- 18-1.py
+++ 18-1.py
@@ -1,8 +1,39 @@
 from pyupbit import WebSocketManager
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Worker(QThread):
+    receiveData = pyqtSignal(dict)
+
+    def run(self):
+        wm = WebSocketManager("ticker", ["KRW-BTC"])
+        self.alive = True
+
+        while self.alive:
+            data = wm.get()
+            self.receiveData.emit(data)
+        wm.terminate()
+
+    def end(self):
+        self.alive = False
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.w = Worker()
+        self.w.receiveData.connect(self.processData)
+        self.w.start()
+
+    def closeEvent(self, event):
+        self.w.end()
+
+    def processData(self, data):
+        print(data)
 
 if __name__ == "__main__":
-    wm = WebSocketManager("ticker", ["KRW-BTC"])
-    for i in range(10):
-        data = wm.get()
-        print(data)
-    wm.terminate()
+    app = QApplication( [] )
+    m = MyWindow()
+    m.show()
+    app.exec_()
