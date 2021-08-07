8ab896fcf9ce6635f1368a09daa326a42091bba3
juicyjung <travelandi01@gmail.com>
2021-08-08 01:46:58 +0900

    chapter 7 mid

--- mySite/Investar/Investar/settings.py
+++ mySite/Investar/Investar/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-with open("upbit_key.txt", "r") as f:
+with open("secret_key.txt", "r") as f:
     key = f.readline().strip()
 
 # SECURITY WARNING: keep the secret key used in production secret!
