2bc3ccc3f73e205feb84ee8545e04694ce82d740
juicyjung <travelandi01@gmail.com>
2021-08-11 15:33:30 +0900

    chapter 7 포기

--- mySite/Investar/Investar/urls.py
+++ mySite/Investar/Investar/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from hello import views
 from django.urls import path, re_path
+from index import views as index_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^(?P<name>[A-Z][a-z]*)$', views.sayHello),
+    path('index/', index_views.main_view),
 ]
