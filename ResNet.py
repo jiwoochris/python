dc38d388c24d741a8369a85653fe9f6b01092e67
Jiwoo Chris Jung <travelandi01@gmail.com>
2023-05-13 15:41:57 +0900

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -1,53 +1,32 @@
 class ResNet(nn.Module):
-
-    def __init__(self, block, layers, **kwargs):
-        self.inplanes = 512
-        self.deconv_with_bias = False
-
+    
+    def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
+        self.in_planes = 64
 
-        self.conv1 = models.resnet18(pretrained=True).conv1
-        self.bn1= models.resnet18(pretrained=True).bn1
-        self.relu= models.resnet18(pretrained=True).relu
-        self.maxpool= models.resnet18(pretrained=True).maxpool
-        self.layer1= models.resnet18(pretrained=True).layer1
-        self.layer2= models.resnet18(pretrained=True).layer2
-        self.layer3= models.resnet18(pretrained=True).layer3
-        self.layer4= models.resnet18(pretrained=True).layer4
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
 
-        self.linear = nn.Linear(512, 40)
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.maxpool2 = nn.AdaptiveMaxPool2d((1,1))
-        
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion, momentum=BN_MOMENTUM),
-            )
-
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x,1)
-        x = self.linear(x)
-        return x
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = torch.nn.functional.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
